de429b33e8d7f60dfd2ef226eb2fb2fe09f4c344
Ganesh K <ganesh799570@gmail.com>
2026-06-30 15:31:22 +0530

    Added the Bootstrap CSS framework for the frontend template pages for the app

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'user1',
+        'title': 'Blog post 1',
+        'content': 'First post Content',
+        'date_posted': 'Aug 27 2026'
+    },
+    {
+        'author': 'user2',
+        'title': 'Blog post 2',
+        'content': 'second post Content',
+        'date_posted': 'Aug 28 2026'
+    },
+]
+
 def blog(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+
+    return render(request, 'blog/about.html', {'title': 'about'})
