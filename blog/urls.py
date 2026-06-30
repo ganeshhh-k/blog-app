ef5fc43ef7c716e80482407e7ca9b3f4653f83b1
Ganesh K <ganesh799570@gmail.com>
2026-06-30 14:59:25 +0530

    Created render templates using html pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog-home')
+    path('home/', views.blog, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
