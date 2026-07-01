f96264e71388b01ec5ad4e94bc9c290d85e92d25
Ganesh K <ganesh799570@gmail.com>
2026-07-01 21:32:58 +0530

    Created posts using python shell and displayed them in the home page

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'user1',
-        'title': 'Blog post 1',
-        'content': 'First post Content',
-        'date_posted': 'Aug 27 2026'
-    },
-    {
-        'author': 'user2',
-        'title': 'Blog post 2',
-        'content': 'second post Content',
-        'date_posted': 'Aug 28 2026'
-    },
-]
+from .models import Post
 
 def blog(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
